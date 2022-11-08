ca5db6fa2f9045a1671b4c89c5c6b4317cf918b9
David Ohayon <davidohayon@turtlefactoryinc.com>
2022-11-08 15:34:37 +0000

    We now have a lazy loaded list to show (dummy) Restaurant Cards in restaurants.screen.js

--- src/features/restaurants/screens/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, SafeAreaView, StatusBar, StyleSheet } from "react-native";
+import { View, SafeAreaView, StatusBar, StyleSheet, FlatList } from "react-native";
 import { Searchbar } from "react-native-paper";
 import { RestaurantInfoCard } from "../components/restaurant-info-card.component";
 import styled from "styled-components/native";
@@ -9,11 +9,11 @@ const SearchContainer = styled(View)`
   padding: ${(props) => props.theme.space[3]};
 `;
 
-const RestaurantListContainer = styled(View)`
-  background-color: ${(props) => props.theme.colors.bg.primary};
-  flex: 1;
-  padding: ${(props) => props.theme.space[3]};
-`;
+const RestaurantList = styled(FlatList).attrs({
+  contentContainerStyle: {
+    padding: 16,
+  },
+})``;
 
 export const RestaurantsScreen = () => {
   return (
@@ -22,9 +22,23 @@ export const RestaurantsScreen = () => {
         <SearchContainer>
           <Searchbar placeholder="Search" />
         </SearchContainer>
-        <RestaurantListContainer>
-          <RestaurantInfoCard />
-        </RestaurantListContainer>
+        <RestaurantList
+          data={[
+            { name: 1 },
+            { name: 2 },
+            { name: 3 },
+            { name: 4 },
+            { name: 5 },
+            { name: 6 },
+            { name: 7 },
+            { name: 8 },
+            { name: 9 },
+            { name: 10 },
+          ]}
+          renderItem={() => <RestaurantInfoCard />}
+          keyExtractor={(item) => item.name}
+          contentContainerStyle={{ padding: 16 }}
+        />
       </SafeAreaView>
     </>
   );
